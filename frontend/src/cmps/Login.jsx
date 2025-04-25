ee448f1bf552109137c42ab97ccbef318d7dfcb1
ReactRay <raydiaz1899@gmail.com>
2025-04-25 12:42:36 +0300

    add styles to login modal

--- frontend/src/cmps/Login.jsx
+++ frontend/src/cmps/Login.jsx
@@ -14,32 +14,43 @@ export function Login() {
     };
 
     return (
-        <div className="login">
-            <form className="form">
-                <div className="form-group">
-                    <input
-                        type="text"
-                        name="username"
-                        placeholder="Username"
-                        value={formData.username}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div className="form-group">
-                    <input
-                        type="password"
-                        name="password"
-                        placeholder="Password"
-                        value={formData.password}
-                        onChange={handleChange}
-                    />
-                </div>
-                <button type="submit">Login</button>
-            </form>
+        <section className='login-modal'>
+            <div className="login">
+                <div className='form-container'>
 
-            <div>
-                <img src="/ai5.jpg" alt="img" />
+
+                    <form className="form">
+                        <div className="form-group">
+                            <input
+                                type="text"
+                                name="username"
+                                placeholder="Username"
+                                value={formData.username}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <input
+                                type="password"
+                                name="password"
+                                placeholder="Password"
+                                value={formData.password}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <button className='btn' type="submit">Login</button>
+                    </form>
+                    <div>
+                        <p>don't have an account? <span className='span-link'>Sign up now</span></p>
+
+                    </div>
+                </div>
+
+                <div className='login-image'>
+                    <img src="/ai5.jpg" alt="img" />
+                </div>
             </div>
-        </div>
+            <button className='btn close'>X</button>
+        </section>
     );
 }
